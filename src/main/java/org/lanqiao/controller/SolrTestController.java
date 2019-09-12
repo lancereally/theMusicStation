17fa1cbd13fdbc7bd71265d8b00e69a5e2a7644b
gsq0630 <2820811808@qq.com>
2019-09-12 15:07:00 +0800

    配置solr--

--- src/main/java/org/lanqiao/controller/SolrTestController.java
+++ src/main/java/org/lanqiao/controller/SolrTestController.java
@@ -17,7 +17,7 @@ import java.util.HashSet;
 import java.util.List;
 
 @Controller
-public class SongsController {
+public class SolrTestController {
 
     @Autowired
     private SolrTemplate solrTemplate;
