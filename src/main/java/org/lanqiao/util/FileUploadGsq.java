cdfe3ccd8de9f177314230d9a9ac12ed03d35e0e
gsq0630 <2820811808@qq.com>
2019-09-12 17:17:23 +0800

    获得工具类【文件上传】

---
+++ src/main/java/org/lanqiao/util/FileUploadGsq.java
@@ -0,0 +1,47 @@
+package org.lanqiao.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.UUID;
+
+public class FileUploadGsq {
+
+    public String upload(HttpServletRequest request, MultipartFile file) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        String fileName = file.getOriginalFilename();
+        //获取服务器所在地址
+        String pathval = request.getSession().getServletContext().getRealPath("/");
+        //图片在服务器下的路径
+        String saveFilePath = "images/uploadFile";
+        //寻找或者建立路径
+        File fileDir = new File(pathval + saveFilePath);
+        if (!fileDir.exists()) {
+            fileDir.mkdirs();
+        }
+        fileName = UUID.randomUUID() + fileName.substring(fileName.lastIndexOf("."));
+        try {
+            inputStream = file.getInputStream();
+            outputStream = new FileOutputStream(fileDir + File.separator + fileName);
+            byte[] b = new byte[1024];
+            while ((inputStream.read(b)) != -1) {
+                outputStream.write(b);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "images/uploadFile/" + fileName;
+    }
+}
+
