620cf015fec7928642f70cbb4a89c0ef43db8a37
rea1GG <455700637@qq.com>
2019-09-15 01:21:36 +0800

    歌单创建与显示后端实现——郭长达

---
+++ src/main/java/org/lanqiao/entity/SongList.java
@@ -0,0 +1,95 @@
+package org.lanqiao.entity;
+
+import java.util.Date;
+
+public class SongList {
+    private Integer songlistId;
+
+    private String songlistName;
+
+    private String songlistDescription;
+
+    private String songlistTag;
+
+    private Integer songlistCount;
+
+    private Date songlistCreateTime;
+
+    private Integer songlistCommetNum;
+
+    private Integer songlistKeepNum;
+
+    private String songlistPicUrl;
+
+    public Integer getSonglistId() {
+        return songlistId;
+    }
+
+    public void setSonglistId(Integer songlistId) {
+        this.songlistId = songlistId;
+    }
+
+    public String getSonglistName() {
+        return songlistName;
+    }
+
+    public void setSonglistName(String songlistName) {
+        this.songlistName = songlistName == null ? null : songlistName.trim();
+    }
+
+    public String getSonglistDescription() {
+        return songlistDescription;
+    }
+
+    public void setSonglistDescription(String songlistDescription) {
+        this.songlistDescription = songlistDescription == null ? null : songlistDescription.trim();
+    }
+
+    public String getSonglistTag() {
+        return songlistTag;
+    }
+
+    public void setSonglistTag(String songlistTag) {
+        this.songlistTag = songlistTag == null ? null : songlistTag.trim();
+    }
+
+    public Integer getSonglistCount() {
+        return songlistCount;
+    }
+
+    public void setSonglistCount(Integer songlistCount) {
+        this.songlistCount = songlistCount;
+    }
+
+    public Date getSonglistCreateTime() {
+        return songlistCreateTime;
+    }
+
+    public void setSonglistCreateTime(Date songlistCreateTime) {
+        this.songlistCreateTime = songlistCreateTime;
+    }
+
+    public Integer getSonglistCommetNum() {
+        return songlistCommetNum;
+    }
+
+    public void setSonglistCommetNum(Integer songlistCommetNum) {
+        this.songlistCommetNum = songlistCommetNum;
+    }
+
+    public Integer getSonglistKeepNum() {
+        return songlistKeepNum;
+    }
+
+    public void setSonglistKeepNum(Integer songlistKeepNum) {
+        this.songlistKeepNum = songlistKeepNum;
+    }
+
+    public String getSonglistPicUrl() {
+        return songlistPicUrl;
+    }
+
+    public void setSonglistPicUrl(String songlistPicUrl) {
+        this.songlistPicUrl = songlistPicUrl == null ? null : songlistPicUrl.trim();
+    }
+}
