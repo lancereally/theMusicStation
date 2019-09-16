0cf2a3da4e5825ecb877f7e3121cd8450e4715e1
lancereally <1305303497@qq.com>
2019-09-16 20:25:06 +0800

    9.16启动类

--- src/main/java/org/lanqiao/MusicApplication.java
+++ src/main/java/org/lanqiao/MusicApplication.java
@@ -1,7 +1,11 @@
 package org.lanqiao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class MusicApplication {
