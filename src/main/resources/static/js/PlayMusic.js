03837eecbfe7f30ee4eba4ddd936d51a55bd01c1
rea1GG <455700637@qq.com>
2019-09-18 02:36:37 +0800

    播放器完成可以旋转 ——郭长达

--- src/main/resources/static/js/PlayMusic.js
+++ src/main/resources/static/js/PlayMusic.js
@@ -10,8 +10,6 @@ $(function () {
             songId[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
         }
     }
-
-
     var playSong = new Vue({
         el: "#songInfo",
         data: {
@@ -47,18 +45,59 @@ $(function () {
                                 lrc: playSong.songLyric
                             }]
                         });
+                        var startL =  $("#loopDiv").attr("data-anim");
+                        var endL =$("#loopDiv").attr("class");
                         ap.on('play',function () {
-                            $("div[class='layui-anim']").trigger("click");
+                            $("#loopDiv").attr("class",startL);
+                        });
+                        ap.on('pause',function () {
+                            $("#loopDiv").attr("class",endL);
                         })
                     }
                 })
             }
         }
     });
+    var sComment = new Vue({
+        el:"#songComment",
+        data:{
+            awComment:[],
+            commentList: [],
+            userHeadUrl1:""
+        },
+        methods:{
+            //查询精彩评论
+            getSongAwC:function () {
+                
+            },
+            //查询全部评论
+            getSongComment:function () {
+                
+            },
+            //查询用户头像
+            getUserPic:function () {
+                
+            },
+            //查询全部评论
+            likeComment:function () {
 
+            },
+            //查询精彩评论
+            likeAwComment:function f() {
+                
+            }
+        }
+    });
 
     playSong.getSongInfo();
+    $("#play_btn").click(function () {
+        var music=document.getElementById("aplayer");
+        if(music.paused == true){
+            alert(go)
+        }else{
 
+        }
+    });
 
 
     // ap.on('playing', function () {
