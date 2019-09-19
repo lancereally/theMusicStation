79406556aaafae4c3c9ae408a8261971bfe0d924
GaoTian <396102009@qq.com>
2019-09-19 15:51:24 +0800

    主页渲染

--- src/main/java/org/lanqiao/service/AlbumService.java
+++ src/main/java/org/lanqiao/service/AlbumService.java
@@ -8,4 +8,5 @@ public interface AlbumService {
     public List<Album> selectBySingerId(Integer singerId);
     // Lzc - search
     public List<Album> selectAlbumByInput(String input);
+    public List<Album> select15Album();
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -140,10 +140,64 @@ $(function(){
             }
 
         }
-
     });
+    var Album = new Vue({
+        el:"#album",
+        data:{
+            AlbumSet1:[],
+            AlbumSet2:[],
+            AlbumSet3:[],
+            url:""
+        },
+        methods:{
+
+            getAlbum:function(){
+                Album.AlbumSet=[];
+                $.ajax({
+                    url:"index/albumRecomment/select15Album",
+                    type:"post",
+                    dataType:"json",
+                    success:function(data){
+                        if (data.length > 0) {
+                            for (var i = 0; i < data.length; i++) {
+                                if(i>=0&&i<5) {
+                                    Album.AlbumSet1.push({
+                                        albumId: data[i].albumId,
+                                        albumName: data[i].albumName,
+                                        albumPicUrl: data[i].albumPicUrl,
+                                        singer: data[i].singer
+                                    });
+                                }
+                                else if(i>=5&&i<10){
+                                    Album.AlbumSet2.push({
+                                        albumId: data[i].albumId,
+                                        albumName: data[i].albumName,
+                                        albumPicUrl: data[i].albumPicUrl,
+                                        singer: data[i].singer
+                                    });
+                                }
+                                else{
+                                    Album.AlbumSet3.push({
+                                        albumId: data[i].albumId,
+                                        albumName: data[i].albumName,
+                                        albumPicUrl: data[i].albumPicUrl,
+                                        singer: data[i].singer
+                                    });
+                                }
+                                // if(i<3)
+                                //     $("span[class='no']").css(" color","#c10d0c");
+                            }
+                        } else {
+                            // alert("表中无记录");
+                        }
+                    }
+                })
+            }
+        }
+    })
     hotSongList.getSongList();
     Leaderboard1.getSong();
     Leaderboard2.getSong();
     Leaderboard3.getSong();
+    Album.getAlbum();
 })
